55a4c4d3def0375bd25753bad2eee0b09f7d32d4
objektwerks <objektwerks@runbox.com>
2021-08-24 07:06:50 -0400

    cleaned up greeter server

--- src/main/scala/objektwerks/GreeterServer.scala
+++ src/main/scala/objektwerks/GreeterServer.scala
@@ -19,7 +19,8 @@ import scala.util.{Failure, Success}
 
 object GreeterServer {
   def main(args: Array[String]): Unit = {
-    val conf = ConfigFactory.parseString("akka.http.server.preview.enable-http2 = on")
+    val conf = ConfigFactory
+      .parseString("akka.http.server.preview.enable-http2 = on")
       .withFallback(ConfigFactory.defaultApplication())
     val system = ActorSystem[Nothing](Behaviors.empty, "GreeterServer", conf)
     new GreeterServer(system).run()
@@ -31,21 +32,19 @@ class GreeterServer(system: ActorSystem[_]) {
     implicit val sys: ActorSystem[_] = system
     implicit val ec: ExecutionContext = system.executionContext
 
-    val service: HttpRequest => Future[HttpResponse] =
-      GreeterServiceHandler(new GreeterServiceImpl(system))
+    val service: HttpRequest => Future[HttpResponse] = GreeterServiceHandler(new GreeterServiceImpl(system))
 
     val bound: Future[Http.ServerBinding] = Http(system)
       .newServerAt(interface = "127.0.0.1", port = 8080)
       .enableHttps(serverHttpContext)
       .bind(service)
       .map(_.addToCoordinatedShutdown(hardTerminationDeadline = 10.seconds))
-
     bound.onComplete {
       case Success(binding) =>
         val address = binding.localAddress
         println("gRPC server bound to {}:{}", address.getHostString, address.getPort)
-      case Failure(ex) =>
-        println("Failed to bind gRPC endpoint, terminating system", ex)
+      case Failure(error) =>
+        println("Failed to bind gRPC endpoint, terminating system ...", error)
         system.terminate()
     }
     bound
