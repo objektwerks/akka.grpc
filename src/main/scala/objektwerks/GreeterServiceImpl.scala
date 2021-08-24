0003d8120fd656520ef32bff24e1f135daed47c1
objektwerks <objektwerks@runbox.com>
2021-08-24 07:09:02 -0400

    cleaned up greeter service impl

--- src/main/scala/objektwerks/GreeterServiceImpl.scala
+++ src/main/scala/objektwerks/GreeterServiceImpl.scala
@@ -7,17 +7,16 @@ import akka.stream.scaladsl.{BroadcastHub, Keep, MergeHub, Sink, Source}
 import scala.concurrent.Future
 
 class GreeterServiceImpl(system: ActorSystem[_]) extends GreeterService {
-  private implicit val sys: ActorSystem[_] = system
+  implicit val sys: ActorSystem[_] = system
 
   val (inboundHub: Sink[HelloRequest, NotUsed], outboundHub: Source[HelloReply, NotUsed]) =
-    MergeHub.source[HelloRequest]
-    .map(request => HelloReply(s"Hello, ${request.name}"))
+    MergeHub
+      .source[HelloRequest]
+      .map(request => HelloReply(s"Hello, ${request.name}"))
       .toMat(BroadcastHub.sink[HelloReply])(Keep.both)
       .run()
 
-  override def sayHello(request: HelloRequest): Future[HelloReply] = {
-    Future.successful(HelloReply(s"Hello, ${request.name}"))
-  }
+  override def sayHello(request: HelloRequest): Future[HelloReply] = Future.successful(HelloReply(s"Hello, ${request.name}"))
 
   override def sayHelloToAll(in: Source[HelloRequest, NotUsed]): Source[HelloReply, NotUsed] = {
     in.runWith(inboundHub)
